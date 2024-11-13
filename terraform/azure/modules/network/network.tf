35c5ddf04e87b01d2c6e60ad095cdb5bab7b8bb7
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 20:53:26 -0300

    Validate

--- terraform/azure/modules/network/network.tf
+++ terraform/azure/modules/network/network.tf
@@ -15,6 +15,7 @@ resource "azurerm_subnet" "snvnetpub1a" {
     resource_group_name  = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes     = ["10.0.1.0/24"]
+    availability_zone = "1"
 }
 
 resource "azurerm_subnet" "snvnetpub1b" {
@@ -22,6 +23,7 @@ resource "azurerm_subnet" "snvnetpub1b" {
     resource_group_name  = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes     = ["10.0.2.0/24"]
+    availability_zone = "2"
 }
 
 resource "azurerm_network_security_group" "nsgvnet" {
