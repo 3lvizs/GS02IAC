18ef28b6c3f9d6278bba29a59d7f245430ae9765
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 20:40:21 -0300

    Validate

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -6,9 +6,7 @@ module "compute" {
  source = "./modules/compute"
   rgname = module.network.rgname
   rglocation = module.network.rglocation
-  vnet10 = module.network.vnet10
-  vnet20 = module.network.vnet20
-  snvnet10pub1a = module.network.snvnet10pub1a
-  snvnet10pub1b = module.network.snvnet10pub1b
-  snvnet20priv = module.network.snvnet20priv
+  vnet = module.network.vnet
+  snvnetpub1a = module.network.snvnetpub1a
+  snvnetpub1b = module.network.snvnetpub1b
 }
