6e9389e453d6a737cc765857adf83de4419b3aac
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 22:20:53 -0300

    Teste

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -4,7 +4,7 @@ module "network" {
 
 module "compute" {
  source = "./modules/compute"
- ec2_ami = module.compute.ec2_ami
+ ec2_ami = "ami-02e136e904f3da870"
  vpc_id = module.network.vpc_id
  sn_vpc_pub1a = module.network.sn_vpc_pub1a
  sn_vpc_pub1b = module.network.sn_vpc_pub1b
