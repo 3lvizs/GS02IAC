62470694afd2faad9717bd393831846fa2023c19
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 21:53:06 -0300

    Teste

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -7,7 +7,6 @@ module "compute" {
   rgname = module.network.rgname
   rglocation = module.network.rglocation
   vnet = module.network.vnet
-  vm_size = module.compute.vm_size
   snvnetpub1a = module.network.snvnetpub1a
   snvnetpub1b = module.network.snvnetpub1b
 }
